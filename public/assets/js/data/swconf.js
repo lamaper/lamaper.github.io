5324ca5fc0b8d41ba88e2f4812a43cd23689e932
lamaper <lamaper@qq.com>
2025-10-31 00:38:36 +0800

    test display

--- public/assets/js/data/swconf.js
+++ public/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1761841509',resources: [
+    cacheName: 'chirpy-1761842265',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
