970f51d8ca76edd6cbaf6587d1fd36e5c1eb56e7
lamaper <lamaper@qq.com>
2025-10-31 00:36:12 +0800

    fix a problem with double same_name dir

--- public/assets/js/data/swconf.js
+++ public/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1761828817',resources: [
+    cacheName: 'chirpy-1761841509',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
