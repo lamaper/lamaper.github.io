272b4ffe3c5abfab9f8b10a910e6c7c30680b305
lamaper <lamaper@qq.com>
2025-11-13 10:50:15 +0800

    a new blog about fastapi trojan

--- public/assets/js/data/swconf.js
+++ public/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1762837545',resources: [
+    cacheName: 'chirpy-1763001943',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
