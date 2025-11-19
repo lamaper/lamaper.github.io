52f3becde004e9f026eb5fccad43bbf3802e62de
lamaper <lamaper@qq.com>
2025-11-19 20:25:03 +0800

    update a new blog about Tornado SSTI

--- public/assets/js/data/swconf.js
+++ public/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1763446638',resources: [
+    cacheName: 'chirpy-1763554995',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
