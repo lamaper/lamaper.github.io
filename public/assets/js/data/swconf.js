1efc007e85ea95e55a1767afbc711def4ddbaa48
lamaper <lamaper@qq.com>
2025-12-08 17:21:42 +0800

    update new blog about backdoorctf

--- public/assets/js/data/swconf.js
+++ public/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1763554995',resources: [
+    cacheName: 'chirpy-1765185634',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
