d71278a5360c81ed0dee0546bf60026f7f9091f6
lamaper <lamaper@qq.com>
2025-12-08 18:15:21 +0800

    update a new blog

--- public/assets/js/data/swconf.js
+++ public/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1765185634',resources: [
+    cacheName: 'chirpy-1765188855',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
