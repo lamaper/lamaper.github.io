03c07b83754d58454c8a24b3f9c69adcc51461be
lamaper <lamaper@qq.com>
2026-01-19 22:13:45 +0800

    new blog about ai

--- public/assets/js/data/swconf.js
+++ public/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1766404223',resources: [
+    cacheName: 'chirpy-1768831984',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
