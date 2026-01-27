66e87c66c158bc585bae01055eb5c4b4d5470a50
lamaper <lamaper@qq.com>
2026-01-27 16:05:21 +0800

    a new about secinfo2026

--- public/assets/js/data/swconf.js
+++ public/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1769320199',resources: [
+    cacheName: 'chirpy-1769501002',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
